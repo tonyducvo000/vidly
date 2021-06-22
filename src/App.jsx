877a33554ebe4e5a596fd4137f92c1f596f1f641
tony <tonyducvo000@gmail.com>
2021-06-22 13:06:04 -0700

    initial mock up - list of movies and info

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Movies from './components/movies'
+import './App.css';
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <main className="container">
+        <Movies></Movies>
+      </main>
+
+
+    );
+  }
+}
+
+export default App;
