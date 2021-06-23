894b5ee59a863c7d2500b254eb7c33c2981c1ef7
tony <tonyducvo000@gmail.com>
2021-06-23 09:26:11 -0700

    added code to handle changes on pagination

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
 import Like from './common/like';
 import Pagination from './common/pagination';
+import { paginate } from '../utils/paginate';
 
 class Movies extends Component {
     state = {
@@ -33,11 +34,12 @@ class Movies extends Component {
     render() {
         //rename length to count
         const { length: count } = this.state.movies;
-        const { pageSize, currentPage } = this.state
+        const { pageSize, currentPage, movies: allMovies } = this.state
         //display only text when empty if this.state.movie.length
         if (count === 0)
             return <p>There are no movies in the database.</p>;
 
+        const movies = paginate(allMovies, currentPage, pageSize);
 
         return (
 
@@ -57,7 +59,7 @@ class Movies extends Component {
 
                     <tbody>
 
-                        {this.state.movies.map(movie => (
+                        {movies.map(movie => (
                             <tr key={movie._id}>
                                 <td>{movie.title}</td>
                                 <td>{movie.genre.name}</td>
