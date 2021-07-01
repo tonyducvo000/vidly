12bcde93ff8b4a51fdd09c65ae115ff5035c770e
tony <tonyducvo000@gmail.com>
2021-07-01 11:19:49 -0700

    added basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import Movies from './components/movies'
+import { Route } from 'react-router-dom';
+import Movies from './components/movies';
+
+
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+
 import './App.css';
 
 class App extends Component {
@@ -8,7 +15,10 @@ class App extends Component {
   render() {
     return (
       <main className="container">
-        <Movies></Movies>
+        <Route path="/movies" component={Movies}></Route>
+        <Route path="/customers" component={Customers}></Route>
+        <Route path="/rentals" component={Rentals}></Route>
+        <Route path="/not-found" component={NotFound}></Route>
       </main>
 
 
