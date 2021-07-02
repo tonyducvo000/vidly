86654731d9e8c4a85a08f629c646e50255293fdb
tony <tonyducvo000@gmail.com>
2021-07-01 18:42:08 -0700

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
+
 import Movies from './components/movies';
-
-
+import NavBar from "./components/navBar"
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
@@ -20,17 +20,19 @@ class App extends Component {
     // Also need to add exact attribute, since hitting /xyz, for example, would redirect
     // users to movies, since /xyx starts with / and this matches from="/"
     return (
-      <main className="container">
-        <Switch>
-          <Route path="/movies" component={Movies}></Route>
-          <Route path="/customers" component={Customers}></Route>
-          <Route path="/rentals" component={Rentals}></Route>
-          <Route path="/not-found" component={NotFound}></Route>
-          <Redirect from="/" exact to="/movies" />
-          <Redirect to="/not-found" />
-        </Switch>
-      </main>
-
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
 
     );
   }
