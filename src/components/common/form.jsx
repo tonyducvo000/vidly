3e1525394dc7282ad6f4bbcd27892b8703af8df9
tony <tonyducvo000@gmail.com>
2021-07-05 17:56:02 -0700

    added button to allow new adding new movie

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Joi from 'joi-browser';
 import Input from './input'
+import Select from './select'
 
 
 class Form extends Component {
@@ -103,6 +104,27 @@ class Form extends Component {
 
         )
     }
+
+
+    renderSelect(name, label, options) {
+        const { data, errors } = this.state;
+
+        return (
+            <Select
+                name={name}
+                value={data[name]}
+                label={label}
+                options={options}
+                onChange={this.handleChange}
+                errors={errors[name]}
+            />
+
+
+        );
+
+
+    }
+
 
     renderInput(name, label, type = 'text') {
 
